8830c50c4ba1f406ac95e789cc1050604123deb2
muhammadali246397 <muhammadali246397@gmail.com>
2023-06-13 23:25:13 +0600

    make user admin api and manageuser api with condition

--- index.js
+++ index.js
@@ -31,13 +31,65 @@ async function run() {
   try {
     const classCollection = client.db('drawing-art').collection('classesCollection')
     const addClsCollection = client.db('drawing-art').collection('addClsCollecton')
+    const userCollection = client.db('drawing-art').collection('userCollection')
 
-    app.get('/class',async(req,res) => {
-      const result = await classCollection.find().sort({available:-1}).toArray()
+    app.post('/users', async(req,res) => {
+      const user = req.body;
+      const query = { userEmail:user.userEmail}
+      const exestingUser = await userCollection.findOne(query)
+      if(exestingUser){
+        return res.send({message:'user allready added'})
+      }
+      const result =await userCollection.insertOne(user);
       res.send(result)
     })
 
-    app.get('/instructor',async(req,res) => {
+    app.get('/users',async(req,res) => {
+      const result = await userCollection.find().toArray();
+      res.send(result)
+    })
+
+    app.patch('/users/admin/:id',async(req,res) => {
+      const id = req.params.id;
+      const filter = {_id: new ObjectId(id)}
+      const options = { upsert: true };
+      const updateDoc = {
+        $set:{
+          role:'admin'
+        }
+      }
+      const result = await userCollection.updateOne(filter,updateDoc,options)
+      res.send(result)
+    })
+
+    app.patch('/users/instructor/:id',async(req,res) => {
+      const id = req.params.id;
+      const filter = {_id: new ObjectId(id)}
+      const options = { upsert: true };
+      const updateDoc = {
+        $set:{
+          role:'instructor'
+        }
+      }
+      const result = await userCollection.updateOne(filter,updateDoc,options)
+      res.send(result)
+    })
+
+    app.get('/class',async(req,res) => {
+      const result = await classCollection.find().limit(6).sort({available:1}).toArray()
+      res.send(result)
+    })
+
+    app.post('/class',async(req,res) => {
+      const data = req.body;
+      if(data.status === 'pending'){
+        return res.send({message:'this class is pending'})
+      }
+      const result = await classCollection.insertOne(data);
+      res.send(result)
+    })
+
+    app.get('/allclass',async(req,res) => {
       const result = await classCollection.find().toArray()
       res.send(result)
     })
@@ -49,6 +101,19 @@ async function run() {
 
     })
 
+
+
+    app.get('/myclass',async(req,res) => {
+      const email = req.query.email;
+      if(!email){
+        res.send([])
+      }
+      const query = {email : email}
+      const result = await classCollection.find(query).toArray()
+      res.send(result)
+    })
+
+
     app.get('/addcls',async(req,res) => {
       const email = req.query.email;
       if(!email){
@@ -59,17 +124,14 @@ async function run() {
       res.send(result)
     })
 
-    // app.get('/addclass',async(req,res) => {
-    //   const result = await addClsCollection.find().toArray()
-    //   res.send(result)
-    // })
-
   app.delete('/addclass/:id',async(req,res) =>{
     const id = req.params.id;
     const query = {_id: new ObjectId(id)}
     const result = await addClsCollection.deleteOne(query)
     res.send(result)
   })
+
+
 
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
